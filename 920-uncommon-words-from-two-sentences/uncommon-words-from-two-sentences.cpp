c7db4b62f3bfb7c1d78ce1134f842da86e225c93
Gopal Dose <97077728+GopalDose@users.noreply.github.com>
2024-09-17 22:38:12 +0530

    Time: 4 ms (40.63%) | Memory: 8.9 MB (45.08%) - LeetSync

---
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        // we need to find unique occurences
+        string s = s1 + " " + s2;
+        unordered_map<string,int> um;
+        string tmp="";
+        for(auto& val : s){ 
+            if(val == ' '){
+                um[tmp]++;
+                tmp = "";
+            }
+            else{
+                tmp += val; // denotes, val = s[i]
+            }
+        }
+        
+        // condition needed if empty string allowed
+        if(tmp != ""){
+            um[tmp]++;
+            tmp = "";
+        }
+
+        // check unique means single count
+        vector<string> ans;
+        for(auto& val:um){ // auto it=um.begin();it<um.end();it++
+
+            // val.first = apple, val.second = 2
+
+            if(val.second==1){
+                ans.emplace_back(val.first); // can use push_back also                
+            }
+
+        }
+
+        return ans;
+    }
+};
