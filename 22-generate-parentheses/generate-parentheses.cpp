972b5eb7efc940466bcc0ac8942d32d16b628bf7
Gopal Dose <97077728+GopalDose@users.noreply.github.com>
2024-11-26 18:10:09 +0530

    Time: 3 ms (70.15%) | Memory: 15.6 MB (31.87%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    void solve(vector<string> &ans, int open, int close, string str){
-        if(open == 0 && close == 0){
+    void solve(vector<string> &ans, int uopen, int uclose,int ropen, int rclose, string str){
+        if(ropen == 0 && rclose == 0){
             ans.push_back(str);
             return;
         }
-
-        if(open > 0){
-            solve(ans, open-1, close, str+'(');
+        if(ropen > 0){
+            solve(ans, uopen+1, uclose, ropen-1, rclose,str+'(');
         }
-        if(open < close){
-            solve(ans, open, close-1,str+ ')');
+        if(uopen > uclose){
+            solve(ans, uopen, uclose+1, ropen, rclose-1 ,str+ ')');
         }
     }
     vector<string> generateParenthesis(int n) {
-        int open = n;
-        int close = n;
+        int uopen = 0;
+        int uclose = 0;
+        int ropen = n;
+        int rclose = n;
         vector<string> ans;
-        solve(ans, open, close, "");
+        solve(ans, uopen, uclose, ropen, rclose,"");
         return ans;
     }
 };
