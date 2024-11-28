2a1d0ceeff93b80bae418c613a2a63da6bc93226
Gopal Dose <97077728+GopalDose@users.noreply.github.com>
2024-11-28 16:18:47 +0530

    Time: 7 ms (7.67%) | Memory: 97.5 MB (5.15%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,29 +1,21 @@
-int init = [] {
-	cin.tie(nullptr)->sync_with_stdio(false);
-    freopen("user.out", "w", stdout);
-
-    for (string s; getline(cin, s);) {
-        int solution = 0, minPrice = INT_MAX;
-        for (int _i = 1, _n = s.length(); _i < _n; ++_i) {
-            int price = s[_i++] & 15;
-            while ((s[_i] & 15) < 10) price = price * 10 + (s[_i++] & 15);
-            minPrice = min(minPrice, price);
-            solution = max(solution, price - minPrice);
-        }
-cout << solution << '\n';    }
-    return 0;
-}();
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int buy =prices[0];
-        int p = 0;
-        for(int i = 0; i<prices.size(); i++){
-            if(prices[i] < buy)
-                buy = prices[i];
-            p = max(p, prices[i] - buy);
+    void solve(vector<int>& prices, int i, int& mini, int& maxi) {
+        if (i == prices.size()) {
+            return;
         }
 
-        return p;
+        if (prices[i] < mini)
+            mini = prices[i];
+        int pro = prices[i] - mini;
+        if (pro > maxi)
+            maxi = pro;
+        solve(prices, i+1, mini, maxi);
+    }
+    int maxProfit(vector<int>& prices) {
+        int mini = INT_MAX;
+        int maxi = INT_MIN;
+        solve(prices, 0, mini, maxi);
+        return maxi;
     }
 };
