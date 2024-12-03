8b36c9824441873fec9ad13745f2b1025604603b
Gopal Dose <97077728+GopalDose@users.noreply.github.com>
2024-12-03 17:17:47 +0530

    Time: 20 ms (53.01%) | Memory: 85.6 MB (18.48%) - LeetSync

---
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        string result;
+        int spaceIndex = 0;
+        int n = s.length();
+        int m = spaces.size();
+        
+        for (int i = 0; i < n; ++i) {
+            if (spaceIndex < m && i == spaces[spaceIndex]) {
+                result += ' ';
+                ++spaceIndex;
+            }
+            result += s[i];
+        }
+        return result;
+    }
+};
