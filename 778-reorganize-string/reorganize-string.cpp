0296efa9b5a6613093a27c4fb6fb1719f59f6e8f
Gopal Dose <97077728+GopalDose@users.noreply.github.com>
2024-12-09 18:16:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (63.99%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,40 +1,71 @@
+class Node {
+public:
+    char data;
+    int count;
+    Node(char ch, int d) {
+        data = ch;
+        count = d;
+    }
+};
+
+class compare {
+public:
+    bool operator()(Node a, Node b) { return a.count < b.count; }
+};
 class Solution {
 public:
     string reorganizeString(string s) {
-        int ch[26] = {0};
-        for(int i = 0; i<s.length(); i++){
-            ch[s[i] - 'a']++;
+        int freq[26] = {0}; // Frequency array for letters 'a' to 'z'
+        string result = "";
+
+        // Count the frequency of each character
+        for (char ch : s) {
+            freq[ch - 'a']++;
         }
 
-        int maxFreq = 0;
-        char maxFrech;
+        // Max heap to store characters based on frequency
+        priority_queue<Node, vector<Node>, compare> maxHeap;
 
-        for(int i = 0; i <26; i++){
-            if(maxFreq < ch[i]){
-                maxFreq = ch[i];
-                maxFrech = 'a' + i;
-            }
-        }
-        int index = 0;
-        while(maxFreq > 0 && index <s.length()){
-            s[index] = maxFrech;
-            index+=2;
-            maxFreq--;
-        }
-
-        if(maxFreq != 0){
-            return "";
-        }
-        ch[maxFrech-'a'] = 0;
-        for(int i = 0; i <26; i++){
-            while(ch[i] > 0){
-                index = index>= s.length() ? 1 : index;
-                s[index] = 'a' + i;
-                ch[i]--;
-                index+=2;
+        // Populate the max heap with characters and their frequencies
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                maxHeap.push(Node('a' + i, freq[i]));
             }
         }
 
-        return s;
+        // Reorganize the string
+        while (maxHeap.size() > 1) {
+            Node first = maxHeap.top();
+            maxHeap.pop();
+            Node second = maxHeap.top();
+            maxHeap.pop();
+
+            // Append the two most frequent characters
+            result += first.data;
+            result += second.data;
+
+            // Decrease their frequencies and push back if still valid
+            first.count--;
+            second.count--;
+            if (first.count > 0) {
+                maxHeap.push(first);
+            }
+            if (second.count > 0) {
+                maxHeap.push(second);
+            }
+        }
+
+        // Handle the last remaining character, if any
+        if (!maxHeap.empty()) {
+            Node last = maxHeap.top();
+            maxHeap.pop();
+            if (last.count == 1) {
+                result += last.data;
+            } else {
+                return ""; // Not possible to reorganize
+            }
+        }
+
+        return result;
     }
 };
