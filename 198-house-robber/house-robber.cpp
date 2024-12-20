a41ce73d1513c181d5861f104a781f9a317efe02
Gopal Dose <97077728+GopalDose@users.noreply.github.com>
2024-12-20 17:01:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (9.43%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int n, vector<int>& dp) {
+        if (n < 0)
+            return 0;
+        if (n == 0)
+            return nums[0];
+        if (dp[n] != -1)
+            return dp[n];
+        int include = solve(nums, n - 2, dp) + nums[n];
+        int exclude = solve(nums, n - 1, dp) + 0;
+        dp[n] = max(include, exclude);
+        return dp[n];
+    }
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size() + 1, -1);
+        return solve(nums, nums.size() - 1, dp);
+    }
+};
