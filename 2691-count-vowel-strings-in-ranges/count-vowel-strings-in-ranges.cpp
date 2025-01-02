f9a605886e33491c2565913b7a64b81e20d2455b
Gopal Dose <97077728+GopalDose@users.noreply.github.com>
2025-01-02 10:03:53 +0530

    Time: 4 ms (81.49%) | Memory: 70.1 MB (53.91%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    bool isVowel(const string& w) {
-        int n = w.length() - 1;
-        return (w[0] == 'a' || w[0] == 'e' || w[0] == 'i' || w[0] == 'o' || w[0] == 'u') &&
-               (w[n] == 'a' || w[n] == 'e' || w[n] == 'i' || w[n] == 'o' || w[n] == 'u');
-    }
-
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
         int n = words.size();
-        vector<int> prefixSum(n, 0);
-
-        // Build the prefix sum array
+        vector<int> Prefix(n + 1, 0);
+        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'}; 
         for (int i = 0; i < n; i++) {
-            prefixSum[i] = (i > 0 ? prefixSum[i - 1] : 0) + (isVowel(words[i]) ? 1 : 0);
+            Prefix[i + 1] = Prefix[i];
+            if (vowels.count(words[i].front()) && vowels.count(words[i].back())) {
+                Prefix[i + 1]++;  
+            }
+        }
+        vector<int> ANS; 
+        for (auto& query : queries) {
+            int L = query[0], R = query[1];  
+            ANS.push_back(Prefix[R + 1] - Prefix[L]);  
         }
 
-        vector<int> results;
-        for (const auto& query : queries) {
-            int l = query[0], r = query[1];
-            int count = prefixSum[r] - (l > 0 ? prefixSum[l - 1] : 0);
-            results.push_back(count);
-        }
-
-        return results;
+        return ANS;  
     }
 };
