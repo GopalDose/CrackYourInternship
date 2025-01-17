5e8b179d9a82065fc0cca275ce1501c93930aa89
Gopal Dose <97077728+GopalDose@users.noreply.github.com>
2025-01-17 22:00:25 +0530

    Time: 0 ms (100.00%) | Memory: 263.9 MB (74.77%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool doesValidArrayExist(vector<int>& derived) {
+    static bool doesValidArrayExist(vector<int>& derived) {
         return accumulate(derived.begin(), derived.end(), 0, bit_xor<>())==0;
     }
 };
