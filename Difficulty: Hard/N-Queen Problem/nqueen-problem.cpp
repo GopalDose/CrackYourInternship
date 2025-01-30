77f758b7969cfe7eaf23c320a1e97e5f5ae07fe8
Gopal Dose <97077728+GopalDose@users.noreply.github.com>
2025-01-30 21:51:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
@@ -4,76 +4,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-public:
-    bool canPut(int row,int col,vector<int> temp){
-        int r = row, c = col;
-        while(r>=0 and c>=0){
-            if(temp[r]==c+1)return false;
-            r--;
-            c--;
-        }
-        r = row, c = col;
-        while(r>=0){
-            if(temp[r]==c+1)return false;
-            r--;
-        }
-        r = row, c = col;
-        while(r>=0 and c<temp.size()){
-            if(temp[r]==c+1)return false;
-            r--;
-            c++;
-        }
-        return true;
-    }
-    void help(int n,vector<vector<int>> &ans,vector<int> temp,int row){
-        if(row==n){
-            ans.push_back(temp);
-            return;
-        }
-        for(int col=0;col<n;col++){
-            if(canPut(row,col,temp)){
-                temp[row]=col+1;
-                help(n,ans,temp,row+1);
-                temp[row]=0;
+class Solution {
+  public:
+    void solve(int colo,int n,vector<vector<int>>&ans,vector<int> &sol,vector<bool>&col,vector<bool>&ldiag,vector<bool>&rdiag){
+        for(int i=0;i<n;i++){
+            if(colo==n){
+                ans.push_back(sol);
+                return ;
+            }
+            
+            if(!col[i] && !ldiag[colo-i + n-1] && !rdiag[i+colo]){
+                sol.push_back(i+1);
+                col[i]=true;
+                ldiag[colo-i + n-1]=true;
+                rdiag[i+colo]=true;
+                
+                solve(colo+1,n,ans,sol,col,ldiag,rdiag);
+                sol.pop_back();
+                col[i]=false;
+                ldiag[colo-i + n-1]=false;
+                rdiag[i+colo]=false;
             }
         }
     }
+    
     vector<vector<int>> nQueen(int n) {
-        // code here
-        vector<vector<int>> ans;
-        vector<int> temp(n,0);
-        help(n,ans,temp,0);
+        //CodeGenius
+        vector<bool>col(n,false);
+        vector<bool>ldiag(2*n-1,false);
+        vector<bool>rdiag(2*n-1,false);
+        vector<vector<int>>ans;
+        vector<int>sol;
+        solve(0,n,ans,sol,col,ldiag,rdiag);
         return ans;
     }
-
 };
 
 //{ Driver Code Starts.
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--) {
         int n;
-        cin>>n;
-        
+        cin >> n;
+
         Solution ob;
         vector<vector<int>> ans = ob.nQueen(n);
-        if(ans.size() == 0)
-            cout<<-1<<"\n";
+        if (ans.size() == 0)
+            cout << -1 << "\n";
         else {
-            for(int i = 0;i < ans.size();i++){
-                cout<<"[";
-                for(int u: ans[i])
-                    cout<<u<<" ";
-                cout<<"] ";
+            sort(ans.begin(), ans.end());
+            for (int i = 0; i < ans.size(); i++) {
+                cout << "[";
+                for (int u : ans[i])
+                    cout << u << " ";
+                cout << "] ";
             }
-            cout<<endl;
+            cout << endl;
         }
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
